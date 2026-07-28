b4d2839e2bff7ae634f415b60f80a8da1ead76a3
Stephane torres <stephane.torres@madetech.com>
2026-07-28 11:21:58 +0100

    introduce fieldset headings

--- citizen_frontend/forms/submission_email.py
+++ citizen_frontend/forms/submission_email.py
@@ -1,11 +1,11 @@
 from crispy_forms_gds.helper import FormHelper
-from crispy_forms_gds.layout import Field, Layout
+from crispy_forms_gds.layout import Fieldset, Layout
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 
 class SubmissionEmailForm(forms.Form):
     email = forms.EmailField(
-        label="Email address",
         help_text="Please provide your email address, "
         "so the licensing authority can let you know if your application has been successful or not.",
         error_messages={
@@ -13,6 +13,7 @@ class SubmissionEmailForm(forms.Form):
             "invalid": "Enter an email address in the correct format, like name@example.com",
         },
         widget=forms.TextInput(attrs={"data-testid": "email-field"}),
+        label="Email address",
     )
 
     confirmation_email = forms.EmailField(
@@ -24,10 +25,25 @@ class SubmissionEmailForm(forms.Form):
         widget=forms.TextInput(attrs={"data-testid": "confirmation-email-field"}),
     )
 
+    application_upload = forms.FileField(
+        help_text="Please attach the completed application form below.",
+        error_messages={"required": "Please provide filled Application form in PDF format"},
+        validators=[FileExtensionValidator(allowed_extensions=["pdf"])],
+        label="Application Form",
+        widget=forms.FileInput(attrs={"data-testid": "application-upload", "class": "govuk-file-upload"}),
+    )
+
     def __init__(self, context):
         super().__init__()
+        self.context = context
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Field.text("email", autocomplete="email"),
-            Field.text("confirmation_email", autocomplete="email"),
+            Fieldset("email", "confirmation_email", legend="Email address", legend_tag="h2", legend_size="s"),
+            Fieldset("application_upload", accept=".pdf", legend="Application form", legend_tag="h2", legend_size="s"),
         )
+
+    # def render_supporting_documents(self):
+    #     for index, supporting_document in enumerate(self.context.supporting_documents):
+    #         supporting_document_upload = forms.FileField(
+    #             label=
+    #         )
--- citizen_frontend/tests/ui/test_licence_submission_page.py
+++ citizen_frontend/tests/ui/test_licence_submission_page.py
@@ -1,4 +1,4 @@
-from conftest import BASE_URL, TEST_TEMP_EVENT_APPLY_FORM_URL
+from conftest import BASE_URL, TEST_FOOD_PREMISES_APPLY_FORM_URL, TEST_TEMP_EVENT_APPLY_FORM_URL
 from playwright.sync_api import Page, expect
 
 
@@ -20,3 +20,21 @@ def test_page_has_confirmation_email_field(page: Page):
     page.goto(f"{BASE_URL}{TEST_TEMP_EVENT_APPLY_FORM_URL}")
 
     expect(page.get_by_test_id("confirmation-email-field")).to_be_visible()
+
+
+def test_page_has_application_form_upload_field(page: Page):
+    page.goto(f"{BASE_URL}{TEST_TEMP_EVENT_APPLY_FORM_URL}")
+
+    expect(page.get_by_test_id("application-upload")).to_be_visible()
+
+
+def test_page_has_additional_file_upload_fields_supporting_documents_required(page: Page):
+    page.goto(f"{BASE_URL}{TEST_FOOD_PREMISES_APPLY_FORM_URL}")
+
+    (
+        expect(page.get_by_test_id("support-documents-statement")).to_contain_text(
+            "All documents are required, unless stated otherwise. "
+            "Photos of documents are acceptable, as long as all the relevant information is clear. "
+            "We'll only share these documents with the licensing authority."
+        )
+    )
