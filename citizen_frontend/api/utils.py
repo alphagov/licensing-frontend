22a0d7deca5dfe12b80cfa8e814523763e470666
Stephane torres <stephane.torres@madetech.com>
2026-09-02 15:48:03 +0100

    get country from gss code

--- citizen_frontend/api/utils.py
+++ citizen_frontend/api/utils.py
@@ -1,12 +1,24 @@
+import re
+
 from common.enums.countries import Countries
 from common.enums.snac_codes import SnacCodes
 from common.models.licences import Licence
 
+# Should these exist in common/go into separate file
 COUNTRY_TO_SNAC_CODE = {
     Countries.ENGLAND: SnacCodes.ENGLAND.value,
     Countries.WALES: SnacCodes.WALES.value,
     Countries.NORTHERN_IRELAND: SnacCodes.NORTHERN_IRELAND.value,
     Countries.SCOTLAND: SnacCodes.SCOTLAND.value,
+}
+
+# should we be doing an external call rather than relying on a regex...
+# How do we even know this is a valid code that exists...
+COUNTRY_TO_GSS_CODE = {
+    Countries.ENGLAND: r"^E\d{8}$",
+    Countries.WALES: r"^W\d{8}$",
+    Countries.NORTHERN_IRELAND: r"^N\d{8}$",
+    Countries.SCOTLAND: r"^S\d{8}$",
 }
 
 
@@ -23,4 +35,9 @@ def get_country_from_geographical_locator(locator: str):
     for key, value in COUNTRY_TO_SNAC_CODE.items():
         if locator in value:
             return key
+
+    for key, value in COUNTRY_TO_GSS_CODE.items():
+        if re.match(value, locator):
+            return key
+
     return None
