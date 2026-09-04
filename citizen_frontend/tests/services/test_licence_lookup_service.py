a256ee7ba9985716eeb9dbfe7d92b83167dbc1ed
amy-a-oconnor <amy.o'connor@madetech.com>
2026-09-04 11:59:31 +0100

    Add failing test for getting local authority url

--- citizen_frontend/tests/services/test_licence_lookup_service.py
+++ citizen_frontend/tests/services/test_licence_lookup_service.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from citizen_frontend.services.licence_lookup_service import LicenceLookupService
 from citizen_frontend.tests.conftest import BASE_URL, TEST_AUTHORITY, TEST_LICENCE
 
@@ -8,3 +10,14 @@ def test_get_base_licence_url_when_authority_uses_gov_uk():
         licence=TEST_LICENCE, authority=TEST_AUTHORITY, uses_gov_uk=True
     )
     assert result == f"{BASE_URL}/apply-for-a-licence/test-licence/test-authority"
+
+
+def test_get_base_licence_url_when_authority_does_not_use_gov_uk():
+    test_authority_not_using_gov_uk = deepcopy(TEST_AUTHORITY)
+    test_authority_not_using_gov_uk.licence_details[0].using_gov_uk = False
+    test_authority_not_using_gov_uk.licence_details[0].authority_url = "test-authority.gov.uk"
+    licence_lookup_service = LicenceLookupService()
+    result = licence_lookup_service.get_base_licence_url(
+        licence=TEST_LICENCE, authority=TEST_AUTHORITY, uses_gov_uk=False
+    )
+    assert result == test_authority_not_using_gov_uk.licence_details[0].authority_url
