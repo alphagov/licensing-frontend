e32299f32c44c79553a41215aa6aa8c3c3503dd8
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-09-08 14:32:59 +0100

    kinda temp again

--- citizen_frontend/repositories/interaction_customisation.py
+++ citizen_frontend/repositories/interaction_customisation.py
@@ -1,37 +1,35 @@
-from licensing_common.common.models.interaction_customisations import InteractionCustomisation
+from common.models.interaction_customisations import InteractionCustomisation
 
 
-class InteractionCustomisationRepository:
-    @staticmethod
-    def _base_find_interaction_customisation(
-        authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
-    ):
-        return InteractionCustomisation.objects.filter(
-            authority_slug_url=authority_slug_url,
-            licence_code=licence_code,
-            interaction_id=interaction_id,
-            interaction_sub_id=interaction_sub_id,
-        )
+def _base_find_interaction_customisation(
+    authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
+):
+    return InteractionCustomisation.objects.filter(
+        authority_slug_url=authority_slug_url,
+        licence_code=licence_code,
+        interaction_id=interaction_id,
+        interaction_sub_id=interaction_sub_id,
+    )
 
-    @classmethod
-    def find_published_interaction_customisation(
-        cls, authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
-    ) -> InteractionCustomisation | None:
-        interaction_with_published_customisation_query = cls._base_find_interaction_customisation(
-            authority_slug_url, licence_code, interaction_id, interaction_sub_id
-        ).filter(published=True)
-        interaction_with_published_customisation = interaction_with_published_customisation_query.first()
-        return (
-            interaction_with_published_customisation.published_customisation
-            if interaction_with_published_customisation
-            else None
-        )
 
-    @classmethod
-    def find_interaction_customisation(
-        cls, authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
-    ) -> InteractionCustomisation | None:
-        customisation = cls._base_find_interaction_customisation(
-            authority_slug_url, licence_code, interaction_id, interaction_sub_id
-        ).first()
-        return customisation
+def find_published_interaction_customisation(
+    authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
+) -> InteractionCustomisation | None:
+    interaction_with_published_customisation_query = _base_find_interaction_customisation(
+        authority_slug_url, licence_code, interaction_id, interaction_sub_id
+    ).filter(published_customisation__isnull=False)
+    interaction_with_published_customisation = interaction_with_published_customisation_query.first()
+    return (
+        interaction_with_published_customisation.published_customisation
+        if interaction_with_published_customisation
+        else None
+    )
+
+
+def find_interaction_customisation(
+    authority_slug_url: str, licence_code: str, interaction_id: int, interaction_sub_id: int
+) -> InteractionCustomisation | None:
+    customisation = _base_find_interaction_customisation(
+        authority_slug_url, licence_code, interaction_id, interaction_sub_id
+    ).first()
+    return customisation
--- citizen_frontend/tests/repositories/test_customisation.py
+++ citizen_frontend/tests/repositories/test_customisation.py
@@ -1,15 +1,20 @@
 import pytest
+from common.models.interaction_customisations import InteractionCustomisation
 
-from citizen_frontend.repositories.interaction_customisation import InteractionCustomisationRepository
+import citizen_frontend.repositories.interaction_customisation as interaction_customisation_repository
 
 
 @pytest.fixture
 def mock_authority_model_filter(mocker):
-    mock_model = mocker.patch(
+    mocker.patch(
         "citizen_frontend.api.repository.interaction_customisation_repository.InteractionCustomisations.objects.filter"
     )
-    yield mock_model
 
 
-def test_find_published_customisation_has_published_constraint(mock_authority_model_filter):
-    InteractionCustomisationRepository.find_published_interaction_customisation("test_slug", "test_liceence_code", 1, 1)
+def test_find_published_customisation_has_published_constraint(mocker):
+    spy = mocker.spy(InteractionCustomisation.objects, "filter")
+    interaction_customisation_repository.find_published_interaction_customisation(
+        "test_slug", "test_licence_code", 1, 1
+    )
+    print(spy.call_args.kwargs)
+    assert spy.call_args.kwargs.get("published_customisation__isnull") is False
