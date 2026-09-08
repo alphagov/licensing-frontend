1b78f9ce7a346732c56aa75cd8570304eac60628
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-09-08 12:34:08 +0100

    additional test fixes

--- citizen_frontend/tests/api/repository/test_authority_repository.py
+++ citizen_frontend/tests/api/repository/test_authority_repository.py
@@ -1,7 +1,7 @@
 import pytest
 from conftest import TEST_LICENCE_CODE
 
-from citizen_frontend.api.repository.authority_repository import AuthorityRepository
+import citizen_frontend.api.repository.authority_repository as authority_repository
 
 
 @pytest.fixture
@@ -13,8 +13,7 @@ def mock_authority_model_filter(mocker):
 def test_get_offering_authorities_by_licence_code_calls_database_with_correct_method_and_args(
     mock_authority_model_filter,
 ):
-    repo = AuthorityRepository()
-    repo.get_licence_offering_authorities_by_licence_code(licence_code=TEST_LICENCE_CODE)
+    authority_repository.get_licence_offering_authorities_by_licence_code(licence_code=TEST_LICENCE_CODE)
 
     mock_authority_model_filter.assert_called_with(
         licence_details__licence_code=TEST_LICENCE_CODE, licence_details__offered_by_authority=True
